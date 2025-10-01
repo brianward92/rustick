c5a9bcc677836f175be7b3c8ba72b9c707984af4
Brian Ward <brian.ward.92@gmail.com>
2025-10-01 19:49:50 -0400

    feat: actual timestamp.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use chrono::Utc;
+
 struct TradeTick {
     ts: i64,
     price: f64,
@@ -5,5 +7,11 @@ struct TradeTick {
 }
 
 fn main() {
-    println!("Hello, world!");
+    let now = Utc::now().timestamp_nanos_opt().expect("Bad timestamp");
+    let trd = TradeTick {
+        ts: now,
+        price: 123.45,
+        size: 5,
+    };
+    println!("Trade at {} at price {} of size {}.", trd.ts, trd.price, trd.size);
 }
