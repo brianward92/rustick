6a35955db75cf30a524e47690284b56eabb2c211
Brian Ward <brian.ward.92@gmail.com>
2025-10-04 12:14:19 -0400

    style: no clunky casting for rounding.

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,8 @@ fn main() {
     let mut rng = rand::thread_rng();
     for i in 1..=10 {
         let now = Utc::now().timestamp_nanos_opt().expect("Bad timestamp");
-        let p = rng.gen_range(95.0..=105.0);
-        let p = (((p * 100.0) as i32) as f64) / 100.0;
+        let p:f64 = rng.gen_range(95.0..=105.0);
+        let p = (p * 100.0).round() / 100.0;
         let s = rng.gen_range(1..=20);
         let trd = TradeTick {
             ts: now,
