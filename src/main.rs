a7614591185a008d56fe56c40aaa5eb2e476458e
Brian Ward <brian.ward.92@gmail.com>
2025-10-04 16:26:21 -0400

    feat: publish to local server. This currently errors if you connect then disconnect. That is because the code does not handle the disconnect error generated.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::io::Write;
+use std::net::TcpListener;
 use std::thread::sleep;
 use std::time::Duration;
 
@@ -10,12 +12,22 @@ struct TradeTick {
     size: u32,
 }
 
-fn main() {
+fn main() -> std::io::Result<()> {
+    // Reserve local address space
+    let server_socket =
+        TcpListener::bind("127.0.0.1:9001").expect("Could not bind to 127.0.0.1:9001");
+    println!("Allocated on 127.0.0.1:9001");
+
+    // Connect from this process
+    let (mut stream, addr) = server_socket.accept()?;
+    println!("Publishing from {}", addr);
+
+    // Publish random prices as ticks
     let mut rng = rand::thread_rng();
     let mut i = 1;
     loop {
         let now = Utc::now().timestamp_nanos_opt().expect("Bad timestamp");
-        let p:f64 = rng.gen_range(95.0..=105.0);
+        let p: f64 = rng.gen_range(95.0..=105.0);
         let p = (p * 100.0).round() / 100.0;
         let s = rng.gen_range(1..=20);
         let trd = TradeTick {
@@ -23,11 +35,18 @@ fn main() {
             price: p,
             size: s,
         };
-        println!(
-            "Trade {} at {} at price {:.2} of size {}.",
+        let msg = format!(
+            "trade_id={} at {} at price {:.2} of size {}.",
             i, trd.ts, trd.price, trd.size
         );
+
+        // Write to 127.0.0.1:9001
+        stream.write_all(msg.as_bytes())?;
+        println!(
+            "Published `trade_id={}` from {} to 127.0.0.1:9001.",
+            i, addr
+        );
         sleep(Duration::from_millis(rng.gen_range(1..=50)));
-        i+= 1;
+        i += 1;
     }
 }
