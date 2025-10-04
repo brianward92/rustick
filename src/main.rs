5a6573e6c0c47d1477c1302b6f00cddcd5884bfa
Brian Ward <brian.ward.92@gmail.com>
2025-10-04 12:07:16 -0400

    feat: actual random prices too.

--- src/main.rs
+++ src/main.rs
@@ -11,11 +11,13 @@ fn main() {
     let mut rng = rand::thread_rng();
     for i in 1..=10 {
         let now = Utc::now().timestamp_nanos_opt().expect("Bad timestamp");
-        let x = rng.gen_range(1..=20);
+        let p = rng.gen_range(95.0..=105.0);
+        let p = (((p * 100.0) as i32) as f64) / 100.0;
+        let s = rng.gen_range(1..=20);
         let trd = TradeTick {
             ts: now,
-            price: 123.45,
-            size: x,
+            price: p,
+            size: s,
         };
         println!(
             "Trade {} at {} at price {} of size {}.",
