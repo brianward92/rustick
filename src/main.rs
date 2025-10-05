4a77fa8fc45d44454b0f9dca1f3da64c4aa1ec2d
Brian Ward <brian.ward.92@gmail.com>
2025-10-04 22:32:31 -0400

    feat: refactor `publish_ticks` into a module.

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,8 @@
-use std::io::Write;
-use std::net::SocketAddr;
 use std::net::TcpListener;
-use std::net::TcpStream;
-use std::thread::sleep;
-use std::time::Duration;
 
-use chrono::Utc;
-use rand::Rng;
-use serde_json;
-
+mod publish_ticks;
 mod tick;
-use crate::tick::TradeTick;
-
-
-fn publish_ticks(mut stream: TcpStream, addr: SocketAddr) -> i64 {
-    // Publish random prices as ticks
-    let mut rng = rand::thread_rng();
-    let mut i: i64 = 1;
-    loop {
-        let now = Utc::now().timestamp_nanos_opt().expect("Bad timestamp");
-        let p: f64 = rng.gen_range(95.0..=105.0);
-        let p = (p * 100.0).round() / 100.0;
-        let s = rng.gen_range(1..=20);
-        let trd = TradeTick {
-            ts: now,
-            price: p,
-            size: s,
-        };
-        let mut msg = serde_json::to_string(&trd).expect("JSON serialization failed.");
-        msg.push('\n');
-
-        // Write to 127.0.0.1:9001
-        println!(
-            "Publishing `trade_id={}` from {} to 127.0.0.1:9001.",
-            i, addr
-        );
-        if let Err(e) = stream.write_all(msg.as_bytes()) {
-            eprintln!("Client disconnected: {}", e);
-            break i;
-        }
-        sleep(Duration::from_millis(rng.gen_range(1..=50)));
-        i += 1;
-    }
-}
+use crate::publish_ticks::publish_ticks;
 
 fn main() -> std::io::Result<()> {
     // Reserve local address space
