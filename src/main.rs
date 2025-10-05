7ba0de6b642cc35e3c50af890c415edd166ab257
Brian Ward <brian.ward.92@gmail.com>
2025-10-05 15:39:48 -0400

    feat: refactor `wait_for_client`.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
+use std::net::SocketAddr;
 use std::net::TcpListener;
+use std::net::TcpStream;
 
 mod publish_ticks;
 mod tick;
 use crate::publish_ticks::publish_ticks;
+
+fn wait_for_client(server_socket: &TcpListener) -> std::io::Result<(TcpStream, SocketAddr)> {
+    println!("Waiting for next client...");
+    let (stream, addr) = server_socket.accept()?;
+    Ok((stream, addr))
+}
 
 fn main() -> std::io::Result<()> {
     // Reserve local address space
@@ -12,15 +20,10 @@ fn main() -> std::io::Result<()> {
 
     // Loop Publisher
     loop {
-        // Wait for extenral process
-        println!("Waiting for next client...");
-        let (stream, addr) = server_socket.accept()?;
-
-        // Someone connnected, time to publish
+        let (stream, addr) = wait_for_client(&server_socket).expect("Failed to connect client!");
         println!("Publishing from {}", addr);
         let nticks: i64 = publish_ticks(stream, addr);
         println!("Published {} tick(s) from {}", nticks, addr);
-
         // They disconnected
     }
 }
